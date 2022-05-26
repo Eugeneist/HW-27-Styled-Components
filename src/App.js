4c449a57db6a123403c77711e53ab6832b4b616b
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2022-05-26 21:46:25 +0300

    added button component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
+import './App.css';
 import Button from "./components/Button";
 
 
 
 function App() {
   return (
-    <div>
+    <div className="App">
       Default styled:
       <Button border="3px solid black" background="red">Click</Button>
       Secondary:
       <Button color="secondary">Click</Button>
       Secondary disabled:
       <Button color="secondary" disabled>Click</Button>
+      Secondary small:
+      <Button color="secondary" size="small">Click</Button>
       Primary:
       <Button color="primary">Click</Button>
       Primary disabled:
       <Button color="primary" disabled>Click</Button>
+      Primary large:
+      <Button color="primary" size="large">Click</Button>
+
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -10,12 +10,28 @@ const StyledButton = styled.button`
     padding: 12px 20px;
     color: ${(props) => props.color || "#fff"};
     cursor: pointer;
-
-    :disabled {
-        background: rgba(203, 212, 230, 0.3);
-        border: 1px solid #7C8DB0;
-        color:#7C8DB0;
-    }
+    ${(props) => {
+        switch (props.size) {
+            case "large":
+                return `
+                    width: 103px;
+                `
+            case "medium":
+                return `
+                    width: 98px;
+                `
+            case "small":
+                return `
+                    width: 90px;
+                    height: 40px;
+                    padding: 8px 16px;
+                `
+            default:
+                return `
+                    width: 98px;
+                `
+        }
+    }};
 `;
 
 const StyledSecondaryButton = styled(StyledButton)`
@@ -23,15 +39,14 @@ const StyledSecondaryButton = styled(StyledButton)`
     background: #fff;
     color: #605DEC;
 
+    :hover {
+        background: #E9E8FC;
+    }
+
     :disabled {
         background: #fff;
         border: 1px solid #7C8DB0;
         color:#7C8DB0;
-    }
-
-
-    :hover {
-        background: #E9E8FC;
     }
 `;
 
@@ -46,20 +61,26 @@ const StyledPrimaryButton = styled(StyledButton)`
         border: 1px solid #1513A0;
     }
 
+    :disabled {
+        background: rgba(203, 212, 230, 0.3);
+        border: 1px solid #7C8DB0;
+        color:#7C8DB0;
+    }
+
 `;
 
 
 const Button = ({ children, color, size, disabled, background, border }) => {
 
     if (color === "secondary") {
-        return <StyledSecondaryButton disabled={disabled}>{children}</StyledSecondaryButton>;
+        return <StyledSecondaryButton size={size} disabled={disabled}>{children}</StyledSecondaryButton>;
     }
 
     if (color === "primary") {
-        return <StyledPrimaryButton disabled={disabled}>{children}</StyledPrimaryButton>;
+        return <StyledPrimaryButton size={size} disabled={disabled}>{children}</StyledPrimaryButton>;
     }
 
-    return <StyledButton color={color} background={background} border={border} disabled={disabled}>{children}</StyledButton>;
+    return <StyledButton size={size} color={color} background={background} border={border} disabled={disabled}>{children}</StyledButton>;
 }
 
 export default Button;
